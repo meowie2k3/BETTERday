98b0e101c8924bc27f5b5ebd0a7bd4ecd52f064e
minhhuydao2003 <55793260+minhhuydao2003@users.noreply.github.com>
2023-05-05 23:41:53 +0700

    complete WelcomePage

--- betterday/lib/WelcomePage.dart
+++ betterday/lib/WelcomePage.dart
@@ -4,6 +4,7 @@
 
 import 'package:betterday/HomeScreen.dart';
 import 'package:flutter/material.dart';
+import 'package:betterday/DustyCircle.dart';
 
 class WelcomePage extends StatelessWidget {
   const WelcomePage({Key? key}) : super(key: key);
@@ -12,172 +13,204 @@ class WelcomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Center(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          SizedBox(
-            width: 350,
-            height: 350,
-            child: Image.network('https://i.imgur.com/OliMevp.png'),
-          ),
-          const Text(
-            'Xin chào Tú!\nHôm nay của bạn thế nào?',
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontFamily: 'Google Sans',
-              fontSize: 23,
-              color: const Color(0xff000000),
-              fontWeight: FontWeight.w700,
-            ),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          Row(
+      body: Stack(children: [
+        Positioned(
+          top: -50,
+          left: -100,
+          child: GradientCircle(),
+        ),
+        Transform.translate(
+          offset: const Offset(0, -50),
+          child: Center(
+              child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               SizedBox(
-                  width: 45,
-                  height: 45,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      print("Rate 1");
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const HomeScreen()),
-                      );
-                    },
-                    style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all<Color>(
-                          const Color(0xFF12D38D)),
-                    ),
-                    child: Transform.scale(
-                      scale:
-                          3.5, // Adjust this value to scale the image up or down
-                      child: Image.network('https://i.imgur.com/UQB0TYm.png'),
-                    ),
+                width: 350,
+                height: 350,
+                child: Image.network('https://i.imgur.com/OliMevp.png'),
+              ),
+              const Text(
+                'Xin chào Tú!\nHôm nay của bạn thế nào?',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontFamily: 'Google Sans',
+                  fontSize: 23,
+                  color: const Color(0xff000000),
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                // ignore: prefer_const_literals_to_create_immutables
+                children: [
+                  const RatingButton(
+                      imageUrl: 'https://i.imgur.com/UQB0TYm.png',
+                      size: 45,
+                      text: 'Tuyệt vời'),
+                  const SizedBox(width: 10),
+                  const RatingButton(
+                      imageUrl: 'https://i.imgur.com/1ufDJ4E.png',
+                      size: 45,
+                      text: 'Vui vẻ'),
+                  const SizedBox(width: 10),
+                  const RatingButton(
+                      imageUrl: 'https://i.imgur.com/Obdcq5i.png',
+                      size: 45,
+                      text: 'Bình thường'),
+                  const SizedBox(width: 10),
+                  const RatingButton(
+                      imageUrl: 'https://i.imgur.com/CQ3tPRC.png',
+                      size: 45,
+                      text: 'Buồn'),
+                  const SizedBox(width: 10),
+                  const RatingButton(
+                      imageUrl: 'https://i.imgur.com/TQFekcJ.png',
+                      size: 45,
+                      text: 'Tệ'),
+                  const SizedBox(width: 10),
+                  const RatingButton(
+                      imageUrl: 'https://i.imgur.com/5637DkS.png',
+                      size: 45,
+                      text: 'Cực tệ'),
+                ],
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              const Text('Hãy chọn theo cảm xúc của bạn...',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontFamily: 'Google Sans',
+                    fontSize: 15,
+                    color: const Color(0xff000000),
+                    fontWeight: FontWeight.w500,
                   )),
-              const SizedBox(width: 5),
-              SizedBox(
-                  width: 45,
-                  height: 45,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      print("Rate 2");
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const HomeScreen()),
-                      );
-                    },
-                    style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all<Color>(
-                          const Color(0xFF12D38D)),
+              const SizedBox(
+                height: 20,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  print("skipped");
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const HomeScreen()),
+                  );
+                },
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all<Color>(
+                      Color.fromARGB(255, 23, 140, 99)),
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
                     ),
-                    child: Transform.scale(
-                      scale:
-                          3.5, // Adjust this value to scale the image up or down
-                      child: Image.network('https://i.imgur.com/1ufDJ4E.png'),
-                    ),
-                  )),
-              const SizedBox(width: 5),
-              SizedBox(
-                  width: 45,
-                  height: 45,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      print("Rate 3");
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const HomeScreen()),
-                      );
-                    },
-                    style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all<Color>(
-                          const Color(0xFF12D38D)),
-                    ),
-                    child: Transform.scale(
-                      scale:
-                          3.5, // Adjust this value to scale the image up or down
-                      child: Image.network('https://i.imgur.com/Obdcq5i.png'),
-                    ),
-                  )),
-              const SizedBox(width: 5),
-              SizedBox(
-                  width: 45,
-                  height: 45,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      print("Rate 4");
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const HomeScreen()),
-                      );
-                    },
-                    style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all<Color>(
-                          const Color(0xFF12D38D)),
-                    ),
-                    child: Transform.scale(
-                      scale:
-                          3.5, // Adjust this value to scale the image up or down
-                      child: Image.network('https://i.imgur.com/CQ3tPRC.png'),
-                    ),
-                  )),
-              const SizedBox(width: 5),
-              SizedBox(
-                  width: 45,
-                  height: 45,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      print("Rate 5");
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const HomeScreen()),
-                      );
-                    },
-                    style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all<Color>(
-                          const Color(0xFF12D38D)),
-                    ),
-                    child: Transform.scale(
-                      scale:
-                          3.5, // Adjust this value to scale the image up or down
-                      child: Image.network('https://i.imgur.com/TQFekcJ.png'),
-                    ),
-                  )),
-              const SizedBox(width: 5),
-              SizedBox(
-                  width: 45,
-                  height: 45,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      print("Rate 6");
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const HomeScreen()),
-                      );
-                    },
-                    style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all<Color>(
-                          const Color(0xFF12D38D)),
-                    ),
-                    child: Transform.scale(
-                      scale:
-                          3.5, // Adjust this value to scale the image up or down
-                      child: Image.network('https://i.imgur.com/5637DkS.png'),
-                    ),
-                  )),
+                  ),
+                ),
+                child: const Text(
+                  'BỎ QUA',
+                  style: TextStyle(
+                    fontFamily: 'Google Sans',
+                    fontSize: 18,
+                    color: const Color(0xffffffff),
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+              )
             ],
-          ),
-        ],
-      )),
+          )),
+        ),
+        const Positioned(
+          top: 450,
+          right: -230,
+          child: DustyCircle(radius: 250),
+        ),
+      ]),
     );
+  }
+}
+
+class GradientCircle extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return CustomPaint(
+      size: const Size(350, 350),
+      painter: _GradientCirclePainter(),
+    );
+  }
+}
+
+class _GradientCirclePainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final center = Offset(size.width / 2, size.height / 2);
+    final radius = size.width / 2;
+
+    const gradient = LinearGradient(
+      colors: [
+        Color(0xff063B28),
+        Color(0xff07D9AD),
+      ],
+      begin: Alignment.topLeft,
+      end: Alignment.bottomRight,
+    );
+
+    final shader = gradient.createShader(
+      Rect.fromCircle(center: center, radius: radius),
+    );
+
+    final paint = Paint()..shader = shader;
+
+    canvas.drawCircle(center, radius, paint);
+  }
+
+  @override
+  bool shouldRepaint(_GradientCirclePainter oldDelegate) => false;
+}
+
+class RatingButton extends StatelessWidget {
+  final String imageUrl;
+  final double size;
+  final String text;
+
+  const RatingButton(
+      {Key? key,
+      required this.imageUrl,
+      required this.size,
+      required this.text})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+        width: size,
+        height: size,
+        child: ElevatedButton(
+            onPressed: () {
+              print(text);
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const HomeScreen()),
+              );
+            },
+            style: ButtonStyle(
+              backgroundColor:
+                  MaterialStateProperty.all<Color>(const Color(0xFF12D38D)),
+              shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+            ),
+            child: Transform.scale(
+              scale: 3.5,
+              child: Image.network(
+                imageUrl,
+                width: size / 2,
+                height: size / 2,
+              ),
+            )));
   }
 }
