0e467d2ad402a3a5eeb41c60bf408469f5748670
dungtruong151 <dungtruong151@gmail.com>
2023-05-10 21:59:18 +0700

    update CourseScreen

--- betterday/lib/CourseScreen.dart
+++ betterday/lib/CourseScreen.dart
@@ -1,36 +1,61 @@
 import 'package:betterday/HomeScreen.dart';
 import 'package:flutter/material.dart';
 
+import 'DustyCircle.dart';
+
 class CourseScreen extends StatelessWidget {
   const CourseScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromARGB(255, 148, 241, 219),
-      appBar: AppBar(
-        title: const Text('Course Screen'),
-        backgroundColor: Colors.transparent,
-        flexibleSpace: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-                colors: [Color(0xFF063B28), Color(0xFF07D9AD)]),
-            borderRadius: BorderRadius.vertical(
-              bottom: Radius.circular(20),
+      //extendBodyBehindAppBar: true,
+      backgroundColor: Color.fromARGB(255, 214, 240, 234),
+      body: SingleChildScrollView(
+        child: Stack(
+          children: <Widget>[
+            const Positioned(
+              top: -200,
+              left: -200,
+              child: DustyCircle(radius: 250),
             ),
-          ),
-        ),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            // Navigate back to the first screen by popping the current route
-            // off the stack.
-            Navigator.pop(context);
-          },
-          child: const Text('Go back!'),
+            const Positioned(
+              top: 450,
+              right: -230,
+              child: DustyCircle(radius: 250),
+            ),
+            Transform.translate(
+              offset: const Offset(0, 0),
+              child: Column(
+                //crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: IconButton(
+                            icon: Transform.scale(
+                              scale: 2.5, // Phóng to 1.5 lần
+                              //alignment: Alignment.topCenter,
+                              child: Image.asset(
+                                  'assets/images/CourseScreen/ReturnButton.png'),
+                            ),
+                            // ignore: avoid_print
+                            onPressed: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) =>
+                                          const HomeScreen()));
+                            }),
+                      ),
+                    ],
+                  ),
+                  //tài nguyên và lắng nghe
+                ],
+              ),
+            ),
+            //child: Stack(children: <Widget>[
+          ],
         ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
@@ -41,21 +66,21 @@ class CourseScreen extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: <Widget>[
             IconButton(
-              icon: const Icon(Icons.home_outlined),
-              onPressed: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const HomeScreen()));
-              },
+              icon: const Icon(Icons.home),
+              onPressed: () {},
             ),
             IconButton(
               icon: const Icon(Icons.favorite_border),
               onPressed: () {},
             ),
             IconButton(
-              icon: const Icon(Icons.bookmark),
-              onPressed: () {},
+              icon: const Icon(Icons.bookmark_border),
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const CourseScreen()));
+              },
             ),
             IconButton(
               icon: const Icon(Icons.chat_bubble_outline),
