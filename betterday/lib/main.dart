e6aae81a8cd99c8e92607cf9e635141961d18e0b
minhhhuydao2003 <daominhhuy.9a1@gmail.com>
2023-05-10 09:33:35 +0700

    test backend chat function

--- betterday/lib/main.dart
+++ betterday/lib/main.dart
@@ -1,7 +1,57 @@
-import 'package:flutter/material.dart';
-import 'package:betterday/WelcomePage.dart';
+/* #############################################################
+# Nhân quả không nợ chúng ta thứ gì, cho nên xin đừng oán giận #
+#                            _                                 #
+#                         _ooOoo_                              #
+#                        o8888888o                             #
+#                        88" . "88                             #
+#                        (| -_- |)                             #
+#                        O\  =  /O                             #
+#                    ____ /'---'\____                          #
+#                   '  \\|       |// '                         #
+#                  '                  '                        #
+#                 /   \\|||  :  |||//  \                       #
+#                /   _||||| -:- |||||_  \                      #
+#                |    | \\\  -  /// |   |                      #
+#                |  \_| '\' --- '/' |   |                      #
+#                \   .-\_ '- . -' _/-.  /                      #
+#               ___'. .' / --.--\  ', ,'___                    #
+#            .""/ <  '.___\_<|>_/___.' _> \"".                 #
+#           | | :  '- \'. ;'. _/; .'/ /  .' ; |                #
+#           \  \ '-.   \_\_'. _.'_/_/  -' _.' /                #
+#============'-.'.__'-. \ \___   /__.-'_.'_.-'=================#
+                           '-=--'
+Đức Phật nơi đây phù hộ code con chạy không Bug. Nam mô a di đà phật
+                        _.-/`)
+                       // / / )
+                    .=// / / / )
+                   // / / / / /
+                  ||         /
+                   \\       /
+                    ))    .'
+                   //    /
+                        / */
 
-void main(List<String> args) {
+import 'package:betterday/WelcomePage.dart';
+import 'package:betterday/shared/constants.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+void main(List<String> args) async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  if (kIsWeb) {
+    //run the initialization for web
+    await Firebase.initializeApp(
+        options: FirebaseOptions(
+            apiKey: Constant.apiKey,
+            appId: Constant.appId,
+            messagingSenderId: Constant.messagingSenderId,
+            projectId: Constant.projectId));
+  } else {
+    await Firebase.initializeApp();
+  }
+
   runApp(const MaterialApp(
     home: MyApp(),
   ));
