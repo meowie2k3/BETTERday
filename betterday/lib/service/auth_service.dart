a3e3db420cd7fdf41e5858e1957c63e31ee5b818
minhhuydao2003 <55793260+minhhuydao2003@users.noreply.github.com>
2023-05-13 11:16:36 +0700

    try auth_service

---
+++ betterday/lib/service/auth_service.dart
@@ -0,0 +1,25 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+
+  //login
+
+  //register
+  Future registerUserWithEmailAndPassword(
+      String fullname, String email, String password) async {
+    try {
+      User user = (await firebaseAuth.createUserWithEmailAndPassword(
+              email: email, password: password))
+          .user!;
+
+      if (user != null) {
+        //call our database service to update user data
+        return true;
+      }
+    } on FirebaseAuthException catch (e) {
+      print(e);
+    }
+  }
+  //sign out
+}
---
+++ betterday/lib/service/database_service.dart
@@ -0,0 +1,9 @@
+class DatabaseService {
+  final String? uid;
+  DatabaseService({this.uid});
+
+  // create reference to the collection
+
+  // updating the userdata
+  Future updateUserData(String fullname, String email) async {}
+}
